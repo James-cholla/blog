e7ab65276abe7ad2f06ef5359b4db92d530bea24
james <james@bromel.cn>
2023-06-25 17:51:01 +0800

    更新主题

---
+++ app.vue
@@ -0,0 +1,10 @@
+<template>
+  <NuxtLayout>
+    <!-- 页面的主要内容在布局系统中添加。 -->
+    <NuxtPage />
+  </NuxtLayout>
+</template>
+
+<script lang="ts">
+
+</script>
---
+++ layouts/default.vue
@@ -0,0 +1,15 @@
+<script lang="ts">
+
+</script>
+
+<template>
+  <main class="main w-9/12 mx-auto flex">
+    <aside class="aside w-20"><Aside></Aside></aside>
+
+    <div>
+      <header></header>
+      <slot />
+    </div>
+  </main>
+  <footer class="footer text-center mt-6">foot</footer>
+</template>
